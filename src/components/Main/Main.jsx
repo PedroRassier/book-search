29837ded67cbdef678f81e8eda993321fe1704af
unknown <pedrorassier@gmail.com>
2022-09-17 17:46:51 -0300

    feature: Google Books API

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,29 @@
-import { booksList } from "../../Data";
+import { useContext, useState } from "react";
 import BookView from "../BookView/BookView";
+
 export default function Main() {
+  const [books, setBooks] = useState({ items: [] });
+  const fetchBooks = () => {
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${"areia"}&download=epub&key=${"AIzaSyAGPOz7bGwxISlKQRAZGYPcid-pMQJHvWI"}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setBooks(data);
+      })
+      .catch((err) => console.error(err));
+  };
   return (
     <div>
-      {booksList.map(({ id, titulo, autor, anoLancamento }) => {
+      {books.items.map((book, index) => {
         return (
           <BookView
-            key={id}
-            title={titulo}
-            autor={autor}
-            anoLancamento={anoLancamento}
+            key={index}
+            title={book.volumeInfo.title}
+            autor={book.volumeInfo.authors}
+            imgSrc={book.volumeInfo.imageLinks.smallThumbnail}
+            description={book.volumeInfo.description}
           />
         );
       })}
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,36 @@
 import { MagnifyingGlass } from "phosphor-react";
+import { useState, createContext } from "react";
 import "./searchBar.scss";
 
 export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const onInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const onSubmitHandler = (e) => {
+    // Prevent browser refreshing after form submission
+    e.preventDefault();
+    // Call fetch books async function
+  };
+  const context = () => {
+    createContext(searchTerm);
+  };
   return (
-    <>
-      <label htmlFor="searchBar">
-        <MagnifyingGlass size={32} />
-        <input type="text" id="searchBar" name="searchBar" />
-      </label>
-    </>
+    <SearchContext.Provider value={searchTerm}>
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor="searchBar">
+          <button type="submit">
+            <MagnifyingGlass size={32} />
+          </button>
+          <input
+            type="text"
+            id="searchBar"
+            value={searchTerm}
+            name="searchBar"
+            onChange={onInputChange}
+          />
+        </label>
+      </form>
+    </SearchContext.Provider>
   );
 }
